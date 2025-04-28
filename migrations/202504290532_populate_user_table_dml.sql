cb331a02d9b6ca645b730a27add642b38b3743af
dungptm <ptmydung101097@gmail.com>
2025-04-29 05:40:13 +0700

    insert data to users table

--- migrations/202504290532_populate_user_table_dml.sql
+++ migrations/202504290532_populate_user_table_dml.sql
@@ -1,6 +1,6 @@
-INSERT INTO users (name, email) VALUES 
-('Alice Johnson', 'alice.johnson@example.com'),
-('Bob Smith', 'bob.smith@example.com'),
-('Charlie Brown', 'charlie.brown@example.com'),
-('Diana Prince', 'diana.prince@example.com'),
-('Ethan Hunt', 'ethan.hunt@example.com');
+INSERT INTO users (id, name, email) VALUES
+(1, 'User 1', 'user1@example.com'),
+(2, 'User 2', 'user2@example.com'),
+(3, 'User 3', 'user3@example.com'),
+(4, 'User 4', 'user4@example.com'),
+(5, 'User 5', 'user5@example.com');
