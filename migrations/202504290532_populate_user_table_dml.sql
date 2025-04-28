ad78951581773e2dc3827678172ede400bd80d14
dungptm <ptmydung101097@gmail.com>
2025-04-29 05:33:33 +0700

    populate table user

---
+++ migrations/202504290532_populate_user_table_dml.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (name, email) VALUES 
+('Alice Johnson', 'alice.johnson@example.com'),
+('Bob Smith', 'bob.smith@example.com'),
+('Charlie Brown', 'charlie.brown@example.com'),
+('Diana Prince', 'diana.prince@example.com'),
+('Ethan Hunt', 'ethan.hunt@example.com');
